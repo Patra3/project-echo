c9418d26743c57e422c193d6069de4b99c25059d
Chen Z. <czheng1968@gmail.com>
2024-11-15 17:51:52 +0000

    basic work on login screen

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
 import postgres from 'postgres';
 
-const sql = postgres('postgres://postgres:zheng1968@chen:5432/tasktodo', {
-
-});
+const sql = postgres('postgres://postgres:zheng1968@chen:5432/tasktodo', {});
 
 export default sql;
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-//sudo -u postgres psql 
+//sudo -u postgres psql <- Personal reference only, this gets a SQL console session
 
+///// APP IMPORTS /////
+import express from 'express';
+import path from 'path';
 import sql from './db.js';
+//////////////////////
 
+
+/*
 await sql`
 CREATE TABLE Persons (
     PersonID int,
@@ -10,4 +16,16 @@ CREATE TABLE Persons (
     Address varchar(255),
     City varchar(255)
 );
-`;
+`;*/
+
+// Host our web app and define our APIs
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
